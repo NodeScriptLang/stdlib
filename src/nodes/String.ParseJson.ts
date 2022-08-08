263d07feaab89b1a36e23f4571b073f9e4087602
Boris Okunskiy <boris@okunskiy.name>
2022-08-08 09:07:06 +0300

    feat: Parse JSON

---
+++ src/nodes/String.ParseJson.ts
@@ -0,0 +1,24 @@
+import { Operator } from '@nodescript/core/types';
+
+export const node: Operator<{ value: string }, unknown> = {
+    metadata: {
+        channel: 'stdlib',
+        name: 'String.ParseJson',
+        version: '1.0.0',
+        tags: ['Data', 'String', 'Parse'],
+        label: 'Parse JSON',
+        description: 'Parses a specified string as  JSON.',
+        keywords: ['string', 'text', 'parse'],
+        params: {
+            value: {
+                schema: {
+                    type: 'string',
+                },
+            }
+        },
+        result: { type: 'any' },
+    },
+    compute(params) {
+        return JSON.parse(params.value);
+    }
+};
