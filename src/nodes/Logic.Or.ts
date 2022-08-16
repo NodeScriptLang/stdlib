2eedb31d69548978ea25a394365efff02989b648
Boris Okunskiy <boris@okunskiy.name>
2022-08-16 20:35:22 +0300

    feat: And & Or

---
+++ src/nodes/Logic.Or.ts
@@ -0,0 +1,32 @@
+import { Operator } from '@nodescript/core/types';
+
+export const node: Operator<{
+    conditions: boolean[];
+}, boolean> = {
+    metadata: {
+        channel: 'stdlib',
+        name: 'Logic.Or',
+        version: '1.0.0',
+        tags: ['Logic', 'Data', 'Boolean'],
+        label: 'And',
+        description: 'Returns true if any of the specified conditions are true, or false otherwise.',
+        keywords: ['or', 'any', 'some'],
+        params: {
+            conditions: {
+                schema: {
+                    type: 'array',
+                    items: { type: 'boolean' },
+                },
+            },
+        },
+        result: { type: 'boolean' },
+    },
+    compute(params) {
+        for (const condition of params.conditions) {
+            if (condition) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
