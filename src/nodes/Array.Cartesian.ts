7a83c847f6dc191797e9bfa562c5da2a174c2b98
Boris Okunskiy <boris@okunskiy.name>
2022-12-05 21:46:00 +0300

    feat: add cartesian

---
+++ src/nodes/Array.Cartesian.ts
@@ -0,0 +1,53 @@
+import { ModuleCompute, ModuleDefinition } from '@nodescript/core/types';
+
+type P = {
+    array: Array<Array<any>>;
+};
+
+type R = any[];
+
+export const module: ModuleDefinition<P, R> = {
+    moduleId: '@stdlib/Array.Cartesian',
+    version: '1.0.2',
+    label: 'Cartesian Product',
+    description: `
+        Computes a Cartesian product of a two-dimensional array.
+    `,
+    keywords: ['combinations'],
+    params: {
+        array: {
+            schema: {
+                type: 'array',
+                items: {
+                    type: 'array',
+                    items: { type: 'any' },
+                },
+            },
+        },
+    },
+    result: {
+        schema: {
+            type: 'array',
+            items: { type: 'any' },
+        }
+    },
+};
+
+export const compute: ModuleCompute<P, R> = params => {
+    const { array } = params;
+    if (array.length === 0) {
+        return [];
+    }
+    let buf: any[][] = array[0].map(_ => [_]);
+    let current: any[][] = [];
+    for (const list of array.slice(1)) {
+        for (const existing of buf) {
+            for (const item of list) {
+                current.push([...existing, item]);
+            }
+        }
+        buf = current;
+        current = [];
+    }
+    return buf;
+};
