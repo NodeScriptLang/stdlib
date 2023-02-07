0be6c7ff992e08ea365db9b7c2bfc07acfcb2232
Boris Okunskiy <boris@okunskiy.name>
2023-02-07 14:04:36 +0300

    feat: add RegExp.Escape

---
+++ src/nodes/RegExp.Escape.ts
@@ -0,0 +1,32 @@
+import { ModuleCompute, ModuleDefinition } from '@nodescript/core/types';
+
+type P = {
+    string: string;
+};
+
+type R = string;
+
+export const module: ModuleDefinition<P, R> = {
+    version: '1.0.0',
+    moduleName: 'RegExp.Escape',
+    description: `
+        Escapes the characters that have special meaning in regular expressions.
+        Used to create regular expressions that match characters like dot, question mark, dollar sign,
+        curly and square brackets, parentheses, etc.
+    `,
+    keywords: ['regex', 'string', 'escape'],
+    params: {
+        string: {
+            schema: { type: 'string' },
+        },
+    },
+    result: {
+        schema: { type: 'string' },
+    }
+};
+
+export const compute: ModuleCompute<P, R> = params => {
+    return params.string
+        .replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+        .replace(/-/g, '\\x2d');
+};
