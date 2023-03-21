a5fe6b36fbcc643a83ad4170641cd4aacefa0260
Boris Okunskiy <boris@okunskiy.name>
2023-03-21 18:01:55 +0300

    chore: return both local and utc date/time

--- src/nodes/Date.Parse.ts
+++ src/nodes/Date.Parse.ts
@@ -6,7 +6,7 @@ type P = { date: any };
 type R = unknown;
 
 export const module: ModuleDefinition<P, R> = {
-    version: '1.0.4',
+    version: '1.0.5',
     moduleName: 'Date.Parse',
     description: 'Returns individual components of the date, i.e. year, month, week, day of month, etc.',
     keywords: ['date', 'string', 'parse'],
@@ -32,6 +32,8 @@ export const compute: ModuleCompute<P, R> = params => {
             dayOfWeek: date.getDay(),
             month: date.getMonth() + 1,
             year: date.getFullYear(),
+            date: formatDate(date.getFullYear(), date.getMonth() + 1, date.getDate()),
+            time: formatTime(date.getHours(), date.getMinutes(), date.getSeconds()),
         },
         utc: {
             milliseconds: date.getUTCMilliseconds(),
@@ -42,12 +44,28 @@ export const compute: ModuleCompute<P, R> = params => {
             dayOfWeek: date.getUTCDay(),
             month: date.getUTCMonth() + 1,
             year: date.getUTCFullYear(),
+            date: formatDate(date.getUTCFullYear(), date.getUTCMonth() + 1, date.getUTCDate()),
+            time: formatTime(date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()),
         },
         iso: date.toISOString(),
         full: date.toUTCString(),
-        date: date.toISOString().replace(/T.*$/gi, ''),
-        time: date.toISOString().replace(/.*T/gi, ''),
         epoch: date.getTime(),
         tzOffset: date.getTimezoneOffset(),
     };
 };
+
+function formatDate(year: number, month: number, day: number) {
+    return [
+        String(year).padStart(4, '0'),
+        String(month).padStart(2, '0'),
+        String(day).padStart(2, '0'),
+    ].join('-');
+}
+
+function formatTime(hours: number, minutes: number, seconds: number) {
+    return [
+        String(hours).padStart(2, '0'),
+        String(minutes).padStart(2, '0'),
+        String(seconds).padStart(2, '0'),
+    ].join(':');
+}
