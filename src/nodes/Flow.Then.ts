32fdade5b9a05bea1e8b7e9d49c095794f67ff88
Boris Okunskiy <boris@okunskiy.name>
2023-03-22 22:01:07 +0300

    feat: Flow.Then

---
+++ src/nodes/Flow.Then.ts
@@ -0,0 +1,40 @@
+import { ModuleCompute, ModuleDefinition } from '@nodescript/core/types';
+
+type P = {
+    step: unknown;
+    then: unknown;
+};
+
+type R = Promise<unknown>;
+
+export const module: ModuleDefinition<P, R> = {
+    version: '1.0.0',
+    moduleName: 'Flow.Then',
+    description: `
+    Runs the specified step, discards its result and a "then" value.
+    Useful when joining mutliple asynchronous processes together.
+    `,
+    keywords: ['flow'],
+    params: {
+        step: {
+            deferred: true,
+            schema: { type: 'any' },
+        },
+        then: {
+            deferred: true,
+            schema: { type: 'any' },
+        },
+    },
+    result: {
+        async: true,
+        schema: {
+            type: 'array',
+            items: { type: 'any' },
+        },
+    }
+};
+
+export const compute: ModuleCompute<P, R> = async (params, ctx) => {
+    await ctx.resolveDeferred(params.step);
+    return await ctx.resolveDeferred(params.then);
+};
