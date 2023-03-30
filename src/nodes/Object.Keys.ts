0b126c59376cb5527fbe57157c4b3713d8b53687
Boris Okunskiy <boris@okunskiy.name>
2023-03-30 21:44:18 +0300

    feat: Object / Keys, Object / Values

---
+++ src/nodes/Object.Keys.ts
@@ -0,0 +1,32 @@
+import { ModuleCompute, ModuleDefinition } from '@nodescript/core/types';
+
+type P = {
+    object: any;
+};
+
+type R = string[];
+
+export const module: ModuleDefinition<P, R> = {
+    version: '1.0.0',
+    moduleName: 'Object / Keys',
+    description: `
+        Creates an array consisting of object keys.
+    `,
+    keywords: ['object', 'keys'],
+    params: {
+        object: {
+            schema: { type: 'any' },
+        },
+    },
+    result: {
+        schema: {
+            type: 'array',
+            items: { type: 'string' },
+        },
+    },
+};
+
+export const compute: ModuleCompute<P, R> = params => {
+    const { object } = params;
+    return Object.keys(object);
+};
---
+++ src/nodes/Object.Values.ts
@@ -0,0 +1,32 @@
+import { ModuleCompute, ModuleDefinition } from '@nodescript/core/types';
+
+type P = {
+    object: any;
+};
+
+type R = any[];
+
+export const module: ModuleDefinition<P, R> = {
+    version: '1.0.0',
+    moduleName: 'Object / Values',
+    description: `
+        Creates an array consisting of object values.
+    `,
+    keywords: ['object', 'values'],
+    params: {
+        object: {
+            schema: { type: 'any' },
+        },
+    },
+    result: {
+        schema: {
+            type: 'array',
+            items: { type: 'any' },
+        },
+    },
+};
+
+export const compute: ModuleCompute<P, R> = params => {
+    const { object } = params;
+    return Object.values(object);
+};
