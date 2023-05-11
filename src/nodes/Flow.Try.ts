ff8018619862a6b8428f1df08cee2a47fe3bf93e
Boris Okunskiy <boris@okunskiy.name>
2023-05-11 14:10:50 +0300

    feat: add Flow.Try, Flow.Fallback no longer handles errors

--- src/nodes/Flow.Try.ts
+++ src/nodes/Flow.Try.ts
@@ -7,9 +7,9 @@ type P = {
 type R = Promise<unknown>;
 
 export const module: ModuleDefinition<P, R> = {
-    version: '1.2.0',
-    moduleName: 'Flow / Fallback',
-    description: 'Runs the steps one-by-one and returns the first non-null result.',
+    version: '1.0.0',
+    moduleName: 'Flow / Try',
+    description: `Runs the steps one-by-one and returns the first result that doesn't throw an error.`,
     keywords: ['flow'],
     params: {
         steps: {
@@ -28,9 +28,12 @@ export const module: ModuleDefinition<P, R> = {
 
 export const compute: ModuleCompute<P, R> = async (params, ctx) => {
     for (const step of params.steps) {
-        const value = await ctx.resolveDeferred(step);
-        if (value != null) {
-            return value;
+        try {
+            return await ctx.resolveDeferred(step);
+        } catch (error: any) {
+            if (error.code === 'EPENDING') {
+                throw error;
+            }
         }
     }
     return null;
