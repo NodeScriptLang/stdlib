23490e2b65136cbe3f3879fdf765ca290bd52066
Boris Okunskiy <boris@okunskiy.name>
2023-10-30 22:42:38 +0300

    chore: bump version

--- src/nodes/Array.Pivot.ts
+++ src/nodes/Array.Pivot.ts
@@ -11,7 +11,7 @@ type P = {
 type R = unknown[];
 
 export const module: ModuleDefinition<P, R> = {
-    version: '1.0.3',
+    version: '1.0.4',
     moduleName: 'Array / Pivot',
     description: `
         Performs grouping and aggregations on an array of objects.
