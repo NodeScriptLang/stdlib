00f71951bf42c59455fa72debe53fcd8e433d230
Boris Okunskiy <boris@okunskiy.name>
2023-12-25 18:41:07 +0300

    chore: use textarea in String

--- src/nodes/String.ts
+++ src/nodes/String.ts
@@ -4,17 +4,22 @@ type P = { value: string };
 type R = string;
 
 export const module: ModuleDefinition<P, R> = {
-    version: '1.0.4',
+    version: '1.0.7',
     moduleName: 'String',
     description: 'Convert value into a string.',
+    resizeMode: 'all',
     params: {
         value: {
             schema: { type: 'string' },
-        }
+            attributes: {
+                renderer: 'textarea',
+                valuePlaceholder: 'Value',
+            },
+        },
     },
     result: {
         schema: { type: 'string' },
-    }
+    },
 };
 
 export const compute: ModuleCompute<P, R> = params => params.value;
