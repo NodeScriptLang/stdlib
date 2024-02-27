f313056acb3b8720e21a8f5e36e23718624b0935
inca <boris@okunskiy.name>
2024-02-27 19:56:52 +0300

    fix: Base64Encode works with typed array views

--- src/nodes/String.Base64Encode.ts
+++ src/nodes/String.Base64Encode.ts
@@ -9,7 +9,7 @@ type P = {
 type R = string;
 
 export const module: ModuleDefinition<P, R> = {
-    version: '1.1.4',
+    version: '1.1.5',
     moduleName: 'String / Base64 Encode',
     description: 'Encodes a UTF-8 string or a binary buffer into a Base64 string.',
     params: {
@@ -39,7 +39,7 @@ export const compute: ModuleCompute<P, R> = (params, ctx) => {
     }
     const isTypedArray = value instanceof Object.getPrototypeOf(Uint8Array);
     if (isTypedArray) {
-        return bufferToBase64(value.buffer, urlMode);
+        return bufferToBase64(value, urlMode);
     }
     const str = ctx.convertType(value, { type: 'string' }) as string;
     return stringToBase64(str, urlMode);
