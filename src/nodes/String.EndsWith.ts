eec544684a4958e9455c51c0aedceb62b167f492
inca <boris@okunskiy.name>
2024-03-26 21:28:29 +0300

    feat: Starts With, Ends With

---
+++ src/nodes/String.EndsWith.ts
@@ -0,0 +1,26 @@
+import { ModuleCompute, ModuleDefinition } from '@nodescript/core/types';
+
+type P = { a: string; b: string };
+type R = boolean;
+
+export const module: ModuleDefinition<P, R> = {
+    version: '1.0.0',
+    moduleName: 'String / Ends With',
+    description: 'Determines whether string "b" appears at the end of string "a".',
+    params: {
+        a: {
+            schema: { type: 'string' },
+        },
+        b: {
+            schema: { type: 'string' },
+        },
+    },
+    result: {
+        schema: { type: 'boolean' },
+    }
+};
+
+export const compute: ModuleCompute<P, R> = params => {
+    const { a, b } = params;
+    return a.endsWith(b);
+};
--- src/nodes/String.Trim.ts
+++ src/nodes/String.Trim.ts
@@ -1,21 +1,45 @@
 import { ModuleCompute, ModuleDefinition } from '@nodescript/core/types';
 
-type P = { string: string };
+type P = {
+    string: string;
+    start: boolean;
+    end: boolean;
+};
 type R = string;
 
 export const module: ModuleDefinition<P, R> = {
-    version: '1.1.4',
+    version: '1.2.0',
     moduleName: 'String / Trim',
     description: 'Removes leading and trailing whitespace from a string.',
     keywords: ['whitespace'],
     params: {
         string: {
             schema: { type: 'string' },
-        }
+        },
+        start: {
+            schema: { type: 'boolean', default: true },
+            advanced: true,
+        },
+        end: {
+            schema: { type: 'boolean', default: true },
+            advanced: true,
+        },
     },
     result: {
         schema: { type: 'string' },
     }
 };
 
-export const compute: ModuleCompute<P, R> = params => params.string.trim();
+export const compute: ModuleCompute<P, R> = params => {
+    const { string, start, end } = params;
+    switch (true) {
+        case start && end:
+            return string.trim();
+        case start:
+            return string.trimStart();
+        case end:
+            return string.trimEnd();
+        default:
+            return string;
+    }
+};
