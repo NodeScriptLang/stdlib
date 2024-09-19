8767c88ce25d539d54526e0cf8e368ab9e830df2
inca <boris@okunskiy.name>
2024-09-19 15:35:58 +0300

    update: @nodescript/core

--- src/nodes/Text.ts
+++ src/nodes/Text.ts
@@ -4,17 +4,15 @@ type P = { value: string };
 type R = string;
 
 export const module: ModuleDefinition<P, R> = {
-    version: '1.0.2',
+    version: '1.0.4',
     moduleName: 'Text',
     description: 'Creates a multi-line string constant.',
     keywords: ['string', 'multiline', 'text'],
-    resizeMode: 'all',
     params: {
         value: {
             schema: { type: 'string' },
             attributes: {
                 renderer: 'textarea',
-                valuePlaceholder: 'Value'
             },
         },
     },
